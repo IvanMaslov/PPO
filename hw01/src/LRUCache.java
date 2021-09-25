12e007e3cf5acdac2d848492793dcb00660f38d3
Ivan <maslov.ivan.2000@niuitmo.ru>
2021-09-25 12:48:25 +0300

    HW01

---
+++ hw01/src/LRUCache.java
@@ -0,0 +1,72 @@
+import lombok.NonNull;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Optional;
+
+public class LRUCache<K, V> {
+
+    private class Node {
+        final K key;
+        final V value;
+        Node next;
+        Node prev;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private void linkNodes(@NonNull Node lhs, @NonNull Node rhs) {
+        lhs.next = rhs;
+        rhs.prev = lhs;
+    }
+
+    private final HashMap<K, Node> storage = new HashMap<>();
+    private final Node fakeNode = new Node(null, null);
+    private Node firstNode = fakeNode;
+    private Node lastNode = fakeNode;
+    public static final int MAX_SIZE = 10;
+
+    private void insertNode(@NonNull Node node) {
+        linkNodes(firstNode, node);
+        firstNode = node;
+    }
+
+    private void removeNode(@NonNull Node node) {
+        Node lhs = node.prev;
+        Node rhs = node.next;
+        assert (lhs != null);
+        if (firstNode == node) {
+            firstNode = firstNode.prev;
+            assert (firstNode != null);
+            return;
+        }
+        linkNodes(lhs, rhs);
+    }
+
+    public void addValue(@NotNull K key, @NotNull V value) {
+        if (storage.size() >= MAX_SIZE) {
+            storage.remove(lastNode.next.key);
+            removeNode(lastNode.next);
+        }
+        Node node = new Node(key, value);
+        insertNode(node);
+        storage.put(key, node);
+    }
+
+    public Optional<V> getValue(@NotNull K key) {
+        if (storage.containsKey(key)) {
+            Node node = storage.get(key);
+            removeNode(node);
+            insertNode(node);
+            storage.put(key, node);
+            return Optional.of(node.value);
+        }
+        return Optional.empty();
+    }
+
+}
---
+++ hw01/test/LRUCacheTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LRUCacheTest {
+    @Test
+    public void simple_test() {
+        LRUCache<Integer, String> cache = new LRUCache<>();
+        cache.addValue(1, "1");
+        assertTrue(cache.getValue(1).isPresent());
+        assertTrue(cache.getValue(2).isEmpty());
+        assertEquals("1", cache.getValue(1).get());
+        cache.addValue(2, "2");
+        assertEquals("2", cache.getValue(2).get());
+        cache.addValue(1, "3");
+        assertEquals("3", cache.getValue(1).get());
+    }
+
+    @Test
+    public void large_test() {
+        LRUCache<Integer, String> cache = new LRUCache<>();
+        for (int i = 0; i < LRUCache.MAX_SIZE; ++i) {
+            cache.addValue(i, String.valueOf(i));
+        }
+        for (int i = LRUCache.MAX_SIZE; i < LRUCache.MAX_SIZE * 2; ++i) {
+            cache.addValue(i, String.valueOf(i));
+            for(int j = i - LRUCache.MAX_SIZE + 1; j <= i; ++j) {
+                assertTrue(cache.getValue(j).isPresent());
+                assertEquals(cache.getValue(j).get(), String.valueOf(j));
+            }
+            for(int j = 0; j < i - LRUCache.MAX_SIZE - 1; ++j) {
+                assertTrue(cache.getValue(j).isEmpty());
+            }
+        }
+    }
+}
