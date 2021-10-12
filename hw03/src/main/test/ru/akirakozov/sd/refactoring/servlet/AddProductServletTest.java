0af262cfcd82887f8018ded23f917760d9bf9572
Ivan <maslov.ivan.2000@niuitmo.ru>
2021-10-12 19:54:25 +0300

    [HW03] Add DB and extend AddProductServletTest

--- hw03/src/main/test/ru/akirakozov/sd/refactoring/servlet/AddProductServletTest.java
+++ hw03/src/main/test/ru/akirakozov/sd/refactoring/servlet/AddProductServletTest.java
@@ -3,9 +3,7 @@ package ru.akirakozov.sd.refactoring.servlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -24,34 +22,25 @@ class AddProductServletTest {
     private static final HttpClient client = HttpClient.newHttpClient();
     private static final Server server = new Server(PORT);
 
-    @BeforeAll
-    public static void initDB() throws Exception {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
+    private final DB db = new DB();
 
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
+    @BeforeAll
+    public static void initServer() {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
         context.addServlet(new ServletHolder(new AddProductServlet()), SERVLET_PATH);
+    }
+
+    @BeforeEach
+    public void initDB() throws Exception {
+        db.init();
         server.start();
     }
 
-    @AfterAll
-    public static void dropDB () throws Exception {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "DROP TABLE PRODUCT";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
+    @AfterEach
+    public void dropDB () throws Exception {
+        db.drop();
         server.stop();
     }
 
@@ -62,6 +51,46 @@ class AddProductServletTest {
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        assertEquals("OK\r\n", response.body());
+        assertEquals(200, response.statusCode());
+    }
+
+    @Test
+    public void noPriceAddProduct() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create("http://localhost:" + PORT + SERVLET_PATH + "?name=phone"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(500, response.statusCode());
+    }
+
+    @Test
+    public void priceAsStringAddProduct() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create("http://localhost:" + PORT + SERVLET_PATH + "?name=phone&price=price"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(500, response.statusCode());
+    }
+
+    @Test
+    public void noNameAddProduct() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create("http://localhost:" + PORT + SERVLET_PATH + "?price=100"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+    }
+
+    @Test
+    public void emptyRequestAddProduct() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create("http://localhost:" + PORT + SERVLET_PATH + "?"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(500, response.statusCode());
     }
 }
